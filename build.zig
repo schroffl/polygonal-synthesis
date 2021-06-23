27395c2dd76568d01a961a60355c545b27914f9a
schroffl <schroffl@users.noreply.github.com>
2021-06-23 17:08:22 +0200

    Update zig-vst to latest version

--- build.zig
+++ build.zig
@@ -15,12 +15,15 @@ pub fn build(b: *std.build.Builder) !void {
     // on windows.
     const target_tag = target.os_tag orelse std.Target.current.os.tag;
 
-    var bundle_step = vst_build.create(b, "zig-poly-synth", "src/main.zig", .{
+    var bundle_step = vst_build.create(b, "src/main.zig", .{
+        .name = "zig-poly-synth",
         .identifier = "org.zig-poly-synth",
         .version = if (target_tag == .windows) null else version,
         .target = target,
         .mode = mode,
     });
 
-    b.default_step.dependOn(&bundle_step.step);
+    var reload_step = bundle_step.autoReload();
+
+    b.default_step.dependOn(&reload_step.step);
 }
